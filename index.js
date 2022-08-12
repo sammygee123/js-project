602e32f7ee0f83e0867887e8ab50ea80653de786
Sammy Gransden <108677712+sammygee123@users.noreply.github.com>
2022-08-12 21:54:51 +0100

    added java script to make counter work

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-// document.getElementById("count").innerText = 5
+let countEl=document.getElementById("count-el")
 
-// let count = 5
-// count = count + 1
-// console.log(count)
+console.log(countEl)
+let count = 0 
 
-// intialize the count as 0
-// listen for clicks on the increment button
-// increment the count variable when the button is clicked
-// change the count-el in the HTML to reflect the new count
-
-
-
-
+function increment(){
+    count = count + 1 
+    countEl.innerText = count
+}
