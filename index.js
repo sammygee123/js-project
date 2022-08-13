0d747040d816ff4c58d4837e6ee56921554ac187
Sammy Gransden <108677712+sammygee123@users.noreply.github.com>
2022-08-13 13:42:14 +0100

    added departure counter and increment and decrement btns for both

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 let countEl=document.getElementById("count-el")
+let countEl2=document.getElementById("count-el2")
 
 console.log(countEl)
 let count = 0 
+
+console.log(countEl2)
+let count2 = 0 
 
 function increment(){
     count = count + 1 
@@ -10,6 +14,15 @@ function increment(){
 function decrement(){
     count = count -1 
     countEl.innerText = count
+}
+
+function increment2(){
+    count2 = count2 + 1 
+    countEl2.innerText = count2
+}
+function decrement2(){
+    count2 = count2 -1 
+    countEl2.innerText = count2
 }
 
 
