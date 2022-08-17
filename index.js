8324672c17082aa4ca576ee3f1a7a42e43c1061e
Sammy Gransden <108677712+sammygee123@users.noreply.github.com>
2022-08-17 20:56:21 +0100

    added save string

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 let countEl=document.getElementById("count-el")
 let countEl2=document.getElementById("count-el2")
+let saveEl = document.getElementById("save-el")
+let saveEl2 = document.getElementById("save-el2")
 
 console.log(countEl)
 let count = 0 
 
 console.log(countEl2)
 let count2 = 0 
+
+console.log(saveEl)
+console.log(saveEl2)
 
 function increment(){
     count = count + 1 
@@ -26,7 +31,14 @@ function decrement2(){
 }
 
 
-function save(){
+function save() {
+    let countStr = count + " - "
+    saveEl.innerText = countStr
     console.log(count)
 }
 
+function save2() {
+    let countStr = count + " - "
+    saveEl2.innerText = countStr
+    console.log(count)
+}
